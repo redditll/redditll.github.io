872c2e4af900c8552b4a8814d7845c48f06a4639
redditll <knyvezmanagement@gmail.com>
2016-02-12 18:41:51 -0500

    Update teams.js

--- javascripts/teams.js
+++ javascripts/teams.js
@@ -1,11 +1,7 @@
-$(document).ready(function() {
-  $('#list').click(function(event) {
-    event.preventDefault();
-    $('#products .item').addClass('list-group-item');
-  });
-  $('#grid').click(function(event) {
-    event.preventDefault();
-    $('#products .item').removeClass('list-group-item');
-    $('#products .item').addClass('grid-group-item');
-  });
+var record = $('.team li');
+
+record.on('click', function() {
+  $(this).toggleClass('flip');
 });
+
+// info and images are from wikipedia
